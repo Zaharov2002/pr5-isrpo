72df65970ae521a2e82eb0fa76c2d02b941ffb96
pavelzack2002@gmail.com <pavelzack2002@gmail.com>
2022-11-17 17:39:45 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr5_isrpo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            cats1.Source = new BitmapImage(new Uri("/cats1.jpg", UriKind.Relative));
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            cats2.Source = new BitmapImage(new Uri("/cats2.jpg", UriKind.Relative));
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            cats3.Source = new BitmapImage(new Uri("/cats3.jpg", UriKind.Relative));
+        }
+    }
+}
